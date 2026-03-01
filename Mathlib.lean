b5d3855513a01d9e3ba313a5adc6ff63f8274957
Nailin Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2026-03-01 07:31:07 +0000

    feat(RingTheory): Module being injective is local property (#31995)
    In this PR, we proved that for module over Noetherian ring, being injective is local property.

--- Mathlib.lean
+++ Mathlib.lean
@@ -6288,6 +6288,7 @@ public import Mathlib.RingTheory.LinearDisjoint
 public import Mathlib.RingTheory.LittleWedderburn
 public import Mathlib.RingTheory.LocalProperties.Basic
 public import Mathlib.RingTheory.LocalProperties.Exactness
+public import Mathlib.RingTheory.LocalProperties.Injective
 public import Mathlib.RingTheory.LocalProperties.IntegrallyClosed
 public import Mathlib.RingTheory.LocalProperties.Projective
 public import Mathlib.RingTheory.LocalProperties.ProjectiveDimension
